0cdd77a8055686af0ebad02864c07963848e969b
Rabin Chaudhary <140046217+sid-rabinc@users.noreply.github.com>
2024-05-17 13:16:06 +1000

    Updated the search service component

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,132 +1,3 @@
-// 'use client';
-
-// import React, { useState } from 'react';
-// import {
-//   Box,
-//   Typography,
-//   Container,
-//   Grid,
-//   TextField,
-//   Button,
-//   Paper,
-//   Hidden,
-//   InputAdornment,
-//   IconButton,
-// } from '@mui/material';
-// import SearchIcon from '@mui/icons-material/Search';
-// import MyLocationIcon from '@mui/icons-material/MyLocation';
-
-// const Hero = () => {
-//   const [location, setLocation] = useState('');
-
-//   const handleLocationSearch = () => {
-//     if (navigator.geolocation) {
-//       navigator.geolocation.getCurrentPosition(
-//         (position) => {
-//           const { latitude, longitude } = position.coords;
-//           // Assuming you have a function to fetch locations
-//           fetchNearbyPractices(latitude, longitude);
-//         },
-//         (error) => {
-//           console.error('Error obtaining location', error);
-//         }
-//       );
-//     } else {
-//       console.error('Geolocation is not supported by this browser.');
-//     }
-//   };
-
-//   const fetchNearbyPractices = (lat, lon) => {
-//     // Placeholder for fetching logic
-//     console.log('Fetching practices near', lat, lon);
-//     // Update the location state or make an API call to get the nearby locations
-//     setLocation(`Lat: ${lat}, Lon: ${lon}`);
-//   };
-
-//   return (
-//     <Box sx={{ py: 8 }}>
-//       <Container maxWidth='lg'>
-//         <Typography
-//           variant='h3'
-//           component='h1'
-//           gutterBottom
-//           textAlign={{ xs: 'center', md: 'left' }}
-//         >
-//           Book your next healthcare appointment
-//         </Typography>
-//         <Typography
-//           variant='h6'
-//           color='textSecondary'
-//           paragraph
-//           textAlign={{ xs: 'center', md: 'left' }}
-//         >
-//           Find, book and add your favourite practitioners to your care team.
-//         </Typography>
-//         <Paper
-//           elevation={3}
-//           sx={{
-//             display: 'flex',
-//             flexDirection: { xs: 'column', md: 'row' },
-//             alignItems: 'center',
-//             padding: 2,
-//             gap: 1,
-//             mt: 4,
-//           }}
-//         >
-//           <TextField
-//             fullWidth
-//             placeholder='Service, practice or practitioner'
-//             variant='outlined'
-//             InputProps={{
-//               startAdornment: (
-//                 <InputAdornment position='start'>
-//                   <SearchIcon />
-//                 </InputAdornment>
-//               ),
-//             }}
-//           />
-//           <TextField
-//             fullWidth
-//             placeholder='Suburb or postcode'
-//             value={location}
-//             variant='outlined'
-//             InputProps={{
-//               startAdornment: (
-//                 <InputAdornment position='start'>
-//                   <IconButton onClick={handleLocationSearch}>
-//                     <MyLocationIcon />
-//                   </IconButton>
-//                 </InputAdornment>
-//               ),
-//             }}
-//           />
-//           <Hidden mdUp>
-//             <Button variant='contained' color='primary' sx={{ width: '100%' }}>
-//               Search
-//             </Button>
-//           </Hidden>
-//           <Hidden mdDown>
-//             <Button
-//               variant='contained'
-//               color='primary'
-//               sx={{
-//                 minWidth: 120, // Ensuring the width aligns with other input fields
-//                 height: 56, // Match the height of the TextFields
-//                 borderRadius: '4px', // Match the border radius of the TextFields
-//                 margin: '0 8px', // Consistent spacing
-//               }}
-//             >
-//               <SearchIcon />
-//             </Button>
-//           </Hidden>
-//         </Paper>
-//       </Container>
-//     </Box>
-//   );
-// };
-
-// export default Hero;
-
 'use client';
 
 import React, { useState } from 'react';
@@ -144,11 +15,15 @@ import {
 import SearchIcon from '@mui/icons-material/Search';
 import LocationOnIcon from '@mui/icons-material/LocationOn';
 import MyLocationIcon from '@mui/icons-material/MyLocation';
+import SearchDropdown from './SearchDropDown';
 
 const Hero = () => {
   const [input, setInput] = useState('');
+  const [service, setService] = useState('');
+  const [dropdownOpen, setDropdownOpen] = useState(false);
 
-  const handleInputChange = (event) => {
+  // Location
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setInput(event.target.value);
   };
 
@@ -176,14 +51,20 @@ const Hero = () => {
     }
   };
 
-  const searchByPostcode = (postcode) => {
+  const searchByPostcode = (postcode: string) => {
     console.log('Searching for postcode:', postcode);
     // Placeholder for API call by postcode
   };
 
-  const searchByCoordinates = (lat, lon) => {
+  const searchByCoordinates = (lat: number, lon: number) => {
     console.log('Fetching practices near', lat, lon);
     // Placeholder for API call by geographic coordinates
+  };
+
+  // Service
+  const handleServiceChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setService(event.target.value);
+    setDropdownOpen(event.target.value !== '');
   };
 
   return (
@@ -216,7 +97,7 @@ const Hero = () => {
             mt: 4,
           }}
         >
-          <TextField
+          {/* <TextField
             fullWidth
             placeholder='Service, practice or practitioner'
             variant='outlined'
@@ -227,7 +108,30 @@ const Hero = () => {
                 </InputAdornment>
               ),
             }}
-          />
+          /> */}
+          <div style={{ position: 'relative', width: '100%' }}>
+            <TextField
+              fullWidth
+              placeholder='Service, practice or practitioner'
+              variant='outlined'
+              value={service}
+              onChange={handleServiceChange}
+              onFocus={() => setDropdownOpen(true)}
+              onBlur={() => setTimeout(() => setDropdownOpen(false), 150)}
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position='start'>
+                    <SearchIcon />
+                  </InputAdornment>
+                ),
+              }}
+            />
+            <SearchDropdown
+              open={dropdownOpen}
+              filter={service}
+              onSelect={(item) => setService(item)}
+            />
+          </div>
           <TextField
             fullWidth
             placeholder='Suburb or postcode'
