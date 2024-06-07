2cdb3caaef5356dca2dcf4cc670d7f5d1c410402
Rabin Chaudhary <140046217+sid-rabinc@users.noreply.github.com>
2024-06-07 10:35:16 +1000

    latest

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,8 +1,15 @@
 import Footer from '@/components/Footer';
 import Header from '@/components/Header';
 import AdminRegistration from '@/components/AdminRegistration';
+import { getServerSession } from 'next-auth/next';
+import { authOptions } from '@/lib/authOptions';
+import { redirect } from 'next/navigation';
 
-export default function AuthPage() {
+export default async function AuthPage() {
+  const session = await getServerSession(authOptions);
+  if (session) {
+    redirect('/dashboard');
+  }
   return (
     <div>
       <Header />
